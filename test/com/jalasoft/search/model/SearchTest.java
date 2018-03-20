58ce5832f003ba84f9ebfc48e9fb5b95c3f25276
Luis Cachi Condori <Luis Cachi@jalasoft.local>
2018-03-20 16:39:16 -0400

    SearchTest first test

--- test/com/jalasoft/search/model/SearchTest.java
+++ test/com/jalasoft/search/model/SearchTest.java
@@ -1,4 +1,16 @@
 package com.jalasoft.search.model;
 
+import org.junit.Test;
+import static org.junit.Assert.assertFalse;
+
 public class SearchTest {
+
+    @Test
+    public void VerifyThisFileDoesnExist(){
+        String fileName = "d:\\prueba\\prueba.txt";
+        Search search = new Search(fileName);
+        assertFalse(search.ExistPath());
+    }
+
+
 }
