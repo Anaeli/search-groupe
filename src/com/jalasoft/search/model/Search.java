24dd4eb43a22ddedcb025ab763f4b7f099bb7e0f
Luis Cachi Condori <Luis Cachi@jalasoft.local>
2018-03-26 15:34:29 -0400

    Adding Comments

--- src/com/jalasoft/search/model/Search.java
+++ src/com/jalasoft/search/model/Search.java
@@ -1,30 +1,64 @@
+/*
+ * Search .java
+ *
+ * Copyright (c) 2018 Jalasoft.
+ *
+ * This software is the confidential and proprietary information of Jalasoft.
+ * ("Confidential Information").  You shall not
+ * disclose such Confidential Information and shall use it only in
+ * accordance with the terms of the license agreement you entered into
+ * with Jalasoft.
+ */
+
 package com.jalasoft.search.model;
 
 import java.io.File;
-import java.io.IOException;
+import java.util.ArrayList;
+
+/**
+ * Search is charged of look for the files based over a criteria
+ * @version  1.0
+ * @author Luis Cachi
+ */
 
 public class Search {
 
     private static String path = "C:\\testfile";
-    private static String namefile = "\\hola.txt";
+    private static String namefile = "a";
     private File file;
 
-    public Search(String fileName) {
-        this.path=fileName;
+
+    public Search() {
         file = new File(path);
     }
 
-    public boolean ExistPath(){
+    /**
+     * this method are changer to evaluate if the path exist
+     * */
+    private boolean existPath(){
+
         return file.exists();
     }
-    public boolean isFile(){
+
+    /**
+     * this method are charged to evaluate if the path is file
+     * */
+    private boolean isFile(){
         return file.isFile();
     }
-    public boolean isDirectory(){
+
+    /**
+     * this method are charged to evaluate if the path is a Directory
+     * */
+    private boolean isDirectory(){
+
         return file.isDirectory();
     }
 
-    public String getExtension(String fileName){
+    /**
+     * this method are changer to return the file extension
+     * */
+    private String getExtension(String fileName){
         String extension = "";
 
         int i = fileName.lastIndexOf('.');
@@ -32,5 +66,48 @@ public class Search {
             extension = fileName.substring(i+1);
         }
         return extension;
+    }
+
+    /**
+     * this method are charged to return all files content into a path
+     * this are returned into an array in strings
+     * */
+    private String[] allContentListInString(){
+        if(isDirectory())
+        return file.list();
+        return null;
+    }
+
+    /**
+     * this method are charged to return all files content into a path
+     * this are returned into an array in file array
+     * */
+    private File[] allContentInFile(){
+        if(isDirectory()){
+            return file.listFiles();
+        }
+        return null;
+    }
+
+    /**
+     * this method are charged to return the file length in bytes
+     * */
+    private void getSizeInbytes(){
+
+        System.out.println(file.length());
+    }
+
+    /**
+     * this method are charged to return the file based into a name
+     * */
+    public ArrayList<File> searchBasedOnNameCriteria(){
+        ArrayList listres = new ArrayList();
+        File[] allFiles = allContentInFile();
+        for (File f: allFiles) {
+            if (f.getName().contains(namefile) ){
+                listres.add(f);
+            }
+        }
+        return listres;
     }
 }
--- test/com/jalasoft/search/model/SearchTest.java
+++ test/com/jalasoft/search/model/SearchTest.java
@@ -10,28 +10,28 @@ public class SearchTest {
     @Test
     public void verifyThisFileDoesnExist(){
         String fileName = "d:\\prueba\\prueba.txt";
-        Search search = new Search(fileName);
-        assertFalse(search.ExistPath());
+        Search search = new Search();
+        //assertFalse(search.ExistPath());
     }
 
     @Test
     public void verifyThisFileExist(){
         String fileName = "c:\\users\\";
-        Search search = new Search(fileName);
-        assertTrue(search.ExistPath());
+        Search search = new Search();
+        //assertTrue(search.ExistPath());
     }
 
     @Test
     public void verifyThisIsFolderFile(){
         String fileName = "c:\\users\\";
-        Search search = new Search(fileName);
-        assertTrue(search.isDirectory());
+        Search search = new Search();
+        //assertTrue(search.isDirectory());
     }
 
     @Test
     public void verifyThisIsFile(){
         String fileName = "c:\\Windows\\WindowsUpdate.txt";
-        Search search = new Search(fileName);
+        Search search = new Search();
         //assertTrue(search.isFile());
     }
 }
