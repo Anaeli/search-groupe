133705bfe41136d2415da8e072f725600cf5885d
lufercc <lufer.lfcc@gmail.com>
2018-04-09 22:29:39 -0400

    Creating the Assets Returning a list of asset deleting an unneccesary class

--- src/com/jalasoft/search/model/FactoryAsset.java
+++ src/com/jalasoft/search/model/FactoryAsset.java
@@ -1,8 +1,45 @@
 package com.jalasoft.search.model;
 
+import java.io.File;
+
 public abstract class  FactoryAsset {
 
-    public static void createAsstets(){
-        
+    /**
+     * this method are changer to return the asset object created
+     * @param asset the File object from where we get the values
+     * @param type The Type passed as string
+     * */
+    public static Asset createAssets(String type, File asset){
+
+        String tempName = asset.getName();
+        String tempPath = asset.getPath();
+        int tempSize = (int) asset.length();
+        boolean hidden = asset.isHidden();
+        boolean readOnly = !asset.canWrite();
+        String tempOwner = "";
+        String extension = getExtension(tempName);
+
+            if (type == "folder") {
+                return new Folder(tempName,tempSize,tempPath,tempOwner, asset.listFiles().length,hidden,readOnly);
+            } else if(type == "file"){
+                return new FileSearch(tempName,tempSize,tempPath,tempOwner,extension,hidden,readOnly);
+
+            }else{
+                return new Asset(tempName,tempSize,tempPath,tempOwner,hidden,readOnly);
+            }
+    }
+
+    /**
+     * this method are changer to return the file extension
+     * */
+    private static String getExtension(String fileName){
+
+        String extension = "";
+
+        int i = fileName.lastIndexOf('.');
+        if (i > 0) {
+            extension = fileName.substring(i+1);
+        }
+        return extension;
     }
 }
